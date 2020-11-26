112a7b5282c54261a396201e9c096e508e1a9281
Fabio Marchesi <fabio.marchesi.99@gmail.com>
2020-11-26 18:19:18 +0100

    progress, still need to add some functions and remove debug output

--- Anno 3/Bioinformatica/gtf-validator/README.md
+++ Anno 3/Bioinformatica/gtf-validator/README.md
@@ -2,15 +2,21 @@
 Python script for the validation of GTF file
 GTF is a file-format fot genomic sequences. More info [here](https://mblab.wustl.edu/GTF22.html#intro)
 Types of errors detected:
- - [x] Diffrent types of strand.
- - [x] Diffrent sources.
  - For each row:
-   - [x] Wrong number of values.
-   - [x] Wrong feature.
-   - [x] Wrong start/end.
-   - [x] Wrong frame.
-   - [x] Wrong Attributes.
-   - [X] Wrong strand.
+   - [x] Wrong number of fields
+   - [x] Feature isn't one of: ```['CDS', 'start_codon', 'stop_codon', '5UTR', '3UTR', 'inter', 'inter_CNS', 'intron_CNS', 'exon']```
+   - [x] Strand is different from ```-``` and ```+```
+   - [x] Frame is not correct. Frame is correct when  it's equal to one of: ```[0, 1, 2]``` when the feature is equal to one of ``` ['CDS', 'start_codon', 'stop_codon']```, otherwise frame must be equal to ```'.'```
+   - [x] The attributes aren't formatted correctly, or gene_id/transcript_id are missing.
+   - [x] ```gene_id``` or ```transcript_id``` are different from ```''``` with feature equal to ```inter``` or ```inter_CNS```.
+ - For all the rows grouped by gene_id:
+   - [x] Different strands.    
+- For each group of entries with the same ```gene_id``` and the same ```transcript_id```:
+  - [X] ```start_codon``` or``` stop_codon``` appears but CDS doesn't or viceversa
+  - [X] lenght of ```start_codon``` isn't 3
+  - [X] lenght of ```stop_codon``` isn't 3
+  - [X] Overlapping ranges for ```start_codon```/```stop_codon```/```CDS```
 
+ 
 Comments in `validator.py` contain more info about each error. `validator.py` works for python 2 and all the next releases.
 The script takes a path to a GTF file as input and creates ```validation.txt``` , a text file with the result of the validation process, in the directory of the script. 
--- Anno 3/Bioinformatica/gtf-validator/validator.py
+++ Anno 3/Bioinformatica/gtf-validator/validator.py
@@ -2,29 +2,17 @@
 # GTF-validator 
 # GTF syntax: <seqname> <source> <feature> <start> <end> <score> <strand> <frame> [attributes] [comments]
 
-import re
+import re, sys
 
 # Validation part:
 # Files and functions for the output of validation's results
-correct_validation = True
 validation = open("validation.txt", "w+")
 def print_error(row, message):
-	correct = False
 	validation.write('Validation failed.\n')
-	validation.write('Row ' + str(row) + ' : ' + message + '\n')
-
-
-# Function check_source(rows)
-# Boolean function that checks whether all the sources of a file are equal
-def check_source(rows):
-	correct = True
-	for i in range(len(rows)):
-		for j in range(len(rows)):
-			if rows[i].split('\t')[1] != rows[j].split('\t')[1]:
-				correct = False;
-				print_error(j, 'Source does not match Source in ' + str(i) + '-th row')
-	return correct
-
+	if row != ' ':
+		validation.write('Row ' + str(row) + ' : ' + message + '\n')
+	else:
+		validation.write(message + '\n')
 
 # Function check_strands(rows)
 # Boolean function that checks whether all the strands of a file are equal
@@ -133,68 +121,180 @@ def list_attributes(s):
 	return attributes		
 
 
-# Function check_attributes(s, i)
+# Function check_attributes(feature, att, i)
 # Boolean function that checks whether the attributes are correct
 # gene_id and transcript_id must be present
-def check_attributes(s, i):
-	elements = list_attributes(s)
+# gene_id and transcript_id must be empty for gene = inter or gene = inter_CNS
+def check_attributes(feature, att, i):
+	elements = list_attributes(att)
 	if elements[0][0] != 'gene_id':
 		print_error(i, 'The first attribute (' + str(elements[0][0]) + ') must be "gene_id"')
 		return False
 	if elements[1][0] != 'transcript_id':
 		print_error(i, 'The second attribute (' + str(elements[1][0]) + ') must be "Transcript_it"')
 		return False
+	if(feature in ['inter', 'inter_CNS'] and (elements[0][1] != '' or elements[1][1] != '')):
+		print_error(i, 'gene_id and transcript_id must be equal to '' for features: inter and inter_CNS')
+		return False
 	return True
 
 
 # Function check_row(row, i)
-# Boolean function that checks whether the ros is correct
+# Boolean function that checks whether the row is correct
 # All the elements of a gtf's file row are checked 
 def check_row(row, i):
 	elements = row.split('\t')
-
+	correct_row = True
 	# Checking the number of values 
 	correct_number_of_values = len(elements) >= 9
 	if correct_number_of_values == False:
 		print_error(i, "Not enough arguments")
 		return False
-
 	# Checking the feature
 	correct_feature = check_feature(elements[2], i) 
-
+	correct_row = correct_row and correct_feature
 	# Checking start and end
 	correct_start_end = check_start_end(elements[3], elements[4], i)
-
+	correct_row = correct_row and correct_start_end
 	# Checking the strand 
 	correct_strand = check_strand(elements[6], i)
-
+	correct_row = correct_row and correct_strand
 	# Checking the frame
 	correct_frame = check_frame(elements[2], elements[7], i)
-
+	correct_row = correct_row and correct_frame
 	# Checking the attributes
-	correct_attributes = check_attributes(elements[8], i)
+	correct_attributes = check_attributes(elements[2], elements[8], i)
+	correct_row = correct_row and correct_attributes
+	return correct_row
 
-	
+
+# Function check_gene_strand(gtf_file_rows)
+# Boolean function which checks if all the strands in the same gene are equal 
+def check_gene_strand(gtf_file_rows):
+	gene_dict = {}
+	for i in range(len(gtf_file_rows)):
+		row = gtf_file_rows[i]
+		if check_row(gtf_file_rows[i], i) == True:
+			elements = row.split('\t')
+			current_strand = elements[6]
+			current_attributes = list_attributes(elements[8])
+			current_gene = current_attributes[0][1]
+			if current_gene in gene_dict.keys():
+				if(gene_dict[current_gene] != current_strand):
+					print_error(i, 'Found a strand that does not match its gene strand')
+					return False
+			else:
+				gene_dict[current_gene] = current_strand
+	return True
+
+
+# Function check_overlap(v)
+# Boolean function that checks wheter or not a list of sorted intervals contains elements that overlap
+def check_overlap(v): 
+	if len(v) == 1:
+		return True
+	for i in range(len(v) - 1):
+		if v[i][1] >= v[i + 1][0]:
+			return False
+	return True
+
+
+# Function check_transcript(transcript)
+# transcript is a sequence of row of the same transcript 
+# for those row we have already checked that the strand is ok
+def check_transcript(transcript):
+	current_attributes = list_attributes(transcript[0].split('\t')[8])
+	current_gene = current_attributes[0][1]
+	current_transcript = current_attributes[1][1]
+	strand = transcript[0].split('\t')[6]
+	print(current_gene)
+	print(current_transcript)
+
+
+	start_codons = []
+	stop_codons = []
+	ranges = []
+	for row in transcript:
+		elements = row.split('\t')
+		start = elements[3]
+		end = elements[4]
+		feature = elements[2]
+		if feature == 'start_codon':
+			start_codons.append((int(start), int(end)))
+		if feature == 'stop_codon':
+			stop_codons.append((int(start), int(end)))
+		if feature == 'CDS':
+			ranges.append((int(start), int(end))) 
+	print(start_codons)
+	print(stop_codons)
+	print(ranges)
+
+	# If there is not a CDS do not controll anything else
+	if len(start_codons) + len(stop_codons) + len(ranges) == 0:
+		return True
+	# If there is a start_codon or stop_codon there must be a CDS and viceversa
+	if len(start_codons) + len(stop_codons) + len(ranges) != 0 and len(start_codons) * len(stop_codons) * len(ranges) == 0:
+		print_error(' ', 'If there is a start_codon or end_codon there must be a CDS and viceversa in transcript ' + current_transcript)
+		return False
+	# Checking if the lenght of start_codon is 3
+	lenght_start_codon = 0
+	for range in start_codons:
+		lenght_start_codon = lenght_start_codon + range[1] - range[0] + 1
+	if lenght_start_codon % 3 != 0:
+		print_error(' ', 'Lenght of start_codon must be a multiple of 3 in transcript ' + current_transcript)
+		return False
+	# Checking if the length of stop_codon is 3
+	lenght_stop_codon = 0
+	for range in stop_codons:
+		lenght_stop_codon = lenght_stop_codon + range[1] - range[0] + 1
+	if lenght_stop_codon % 3 != 0:
+		print_error(' ', 'Lenght of stop_codon must be a multiple of 3 in transcript ' + current_transcript)
+		return False
+	start_codons = sorted(start_codons)
+	stop_codons = sorted(stop_codons)
+	ranges = sorted(ranges)
+	# Checking that ranges do not overlap for start_codons 
+	if check_overlap(start_codons) == False:
+		print_error(' ', 'Start_codons overlap in transcript ' + current_transcript)
+		return False
+	# Checking that ranges do not overlap for stop_codons 
+	if check_overlap(stop_codons) == False:
+		print_error(' ', 'Stop_codons overlap in transcript ' + current_transcript)
+		return False
+	# Checking that ranges do not overlap for CDS ranges 
+	if check_overlap(ranges) == False:
+		print_error(' ', 'CDS ranges overlap in transcript ' + current_transcript)
+		return False
+	# Checking if start_codon is in a correct position 
+
+	print("\n\n")
+
 
 # Reading file's name and its lines
 gtf_file_name = input()
 with open(gtf_file_name, 'r') as gtf_input_file:
 	gtf_file_rows = gtf_input_file.readlines()
+# Checking strands for the same gene
+if check_gene_strand(gtf_file_rows) == False:
+	print_error('-', 'Impossibile to continue the validation bacause of this error')
+	sys.exit(0)
+# Group all the rows by gene and transcript
+gene_transcript_dict = {}
+different_transcripts = []
+for i in range(len(gtf_file_rows)):
+	row = gtf_file_rows[i]
+	elements = row.split('\t')
+	current_attributes = list_attributes(elements[8])
+	current_gene = current_attributes[0][1]
+	current_transcript = current_attributes[1][1]
+	if (current_gene, current_transcript) in gene_transcript_dict.keys():
+		current_index = gene_transcript_dict[(current_gene, current_transcript)]
+		different_transcripts[current_index].append(row)
+	else:
+		gene_transcript_dict[(current_gene, current_transcript)] = len(different_transcripts)
+		different_transcripts.append([row])
 
-# Checking if all sources are equal 
-correct_sources = check_source(gtf_file_rows)
-
-# Checking if all the strands are equal 
-correct_strands = check_strands(gtf_file_rows) 
-
-if correct_strands and correct_sources:
-	for i in range(len(gtf_file_rows)):
-		check_row(gtf_file_rows[i], i)
-else:
-	print_error(0, 'The given errors do not permit the validation of each row')
-
-# Validation Veredict:
-if correct_validation == True:
-	validation.write("VALIDATION COMPLETED SUCCESSFULLY\n")
+for transcript in different_transcripts:
+	check_transcript(transcript)
 
 # py validator.py ./tests/first_test.gtf
