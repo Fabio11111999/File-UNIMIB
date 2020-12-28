f2a92b6178f2a2196138a0521fac13d52202d57a
Fabio Marchesi <fabio.marchesi,99@gmail.com>
2020-12-28 17:24:30 +0100

    Finished converter

---
+++ Anno 3/Bioinformatica/fastq-fasta-converter/README.md
@@ -0,0 +1,25 @@
+
+# FASTQ-FASTA-converter
+Python script for the convertion of a FASTQ file into a FASTA file.
+FASTAQ and FASTA are text-based formats for representing  nucleotide sequences. More info about [FASTA](https://en.wikipedia.org/wiki/FASTA_format) and [FASTQ](https://en.wikipedia.org/wiki/FASTQ_format#:~:text=FASTQ%20format%20is%20a%20text,single%20ASCII%20character%20for%20brevity.).
+
+### How it works:
+ ```converter.py``` is a script that takes a path to a FASTQ file as input and creates ```converted.fa``` , a FASTA file with the result of the conversion process. The script takes other 5 values in input and uses them to select the reads that need to be converted. 
+
+### Requirements 
+For being able to run the script Python need to be present on the computer. It's also needed a file with extension ```.fq``` that needs to be in the directory of the script.
+
+### Documentation:
+**Input:**
+- The name of the ```.fq``` file.
+- ```L1, L2, Q1, Q2, P``` and will convert only the reads that satisfy the following conditions:
+   - The read is longer than ```L1```.
+   - The read isn't longer than ```L2```.
+   - The minimun quality of a base of the read must be greater or equal to ```Q1```.
+   - The longest subregion with quality greater or equal than Q2 is at lest p% of the total length.
+**Output:**
+- ```converted.fa``` a FASTA file which contains the conversion of the reads that satisfy the conditions written before, with some information about each record:
+   - Length.
+   - Minimum quality. 
+   - Average quality.
+   - Start and end indexes of the longest subregion with quality \ge ```Q2```.
--- Anno 3/Bioinformatica/fastq-fasta-converter/converter.py
+++ Anno 3/Bioinformatica/fastq-fasta-converter/converter.py
@@ -2,7 +2,45 @@
 # FASTQ - FASTA converter
 
 from Bio import SeqIO
-import sys, re
+import sys, re, math
+# Function longest_subregion(read, Q2)
+# Function that returns (i, j), where i and j are the limits of the longest subregion 
+# 	with minimum quality greater or equal than Q2
+def longest_subregion(read, Q2):
+	sequence = str(read.seq)
+	i, best_start, best_end = 0, -1, -2 
+	while i < len(sequence):
+		if read.letter_annotations['phred_quality'][i] < Q2:
+			i = i + 1
+			continue
+		j = i
+		while j < len(sequence) and read.letter_annotations['phred_quality'][j] >= Q2:
+			j = j + 1
+		if j - i > best_end - best_start:
+			best_start, best_end = i, j
+		i = j
+	return (best_start, best_end)
+
+
+# Function check_read(read, L1, L2, Q1, Q2, P)
+# Boolean function that checks whether the following conditions are satisfied:
+# 	read is longer than L1
+#	read isn't longer than L2
+# 	read's minimum quality is greater or equal than Q1
+# 	The longest subregion with quality greater or equal than Q2 is at lest p% of the total length .
+def check_read(read, L1, L2, Q1, Q2, P):
+	sequence = str(read.seq)
+	if len(sequence) < L1 or len(sequence) >L2:
+		return False
+	if min(read.letter_annotations['phred_quality']) < Q1:
+		return False
+	subregion = longest_subregion(read, Q2)
+	best_start, best_end = subregion[0], subregion[1]
+	min_length = math.ceil(float(len(sequence)) / 100 * P)
+	if min_length > best_end - best_start:
+		return False
+	return True
+
 
 # Main Function:
 print('Starting FASTQ-FASTA converter')
@@ -10,6 +48,7 @@ try:
 	# Reading input
 	print('Path to the file FASTQ:')
 	FASTQ_file_name = input()
+	fastq_records = SeqIO.parse(FASTQ_file_name, 'fastq')
 	print('Type the minimum read length L1')
 	L1 = int(input())
 	print('Type the maximum read length L2')
@@ -19,13 +58,35 @@ try:
 		sys.exit(0)
 	print('Type the minimum base quality Q1')
 	Q1 = int(input())
-	print('Type Q2 and P, only the reads that contains at least one substring which its length is >= P\% of the total read and its minimum quality is >= Q2 will be keeped')
+	print('Type Q2 and P, only the reads that contain at least one substring which its length is >= P\% of the total read and its minimum quality is >= Q2 will be keeped')
 	Q2 = int(input())
 	if Q1 >= Q2:
 		print('Execution failed, Q1 must be < Q2')
 		sys.exit(0)
-	P = int(input())
+	P = float(input())
 	if 0 > P or 100 < P:
 		print('Execution failed, P must be >= and <= 100')
-except:
+except Exception as e:
+	print(e)
 	print('Execution failed, impossibile to read the input')
+	sys.exit(0)
+
+# Processing reads
+
+output = []
+fastq_record_list = list(fastq_records)
+for read in fastq_record_list:
+	correct = check_read(read, L1, L2, Q1, Q2, P)
+	if correct:
+		read.description = 'Lenght: ' + str(len(read.seq)) + ', '
+		read.description += 'Minimum quality: ' + str(min(read.letter_annotations['phred_quality'])) + ", "
+		subregion = longest_subregion(read, Q2)
+		best_start, best_end = subregion[0], subregion[1]
+		read.description += 'Subregion\'s start: ' + str(best_start) + ', '
+		read.description += 'Subregion\'s end: ' + str(best_end - 1) + ', '
+		sequence_subregion = read.letter_annotations['phred_quality'][best_start : best_end]
+		avg = sum(sequence_subregion) / len(sequence_subregion)
+		read.description += 'Average quality: ' + str(avg)
+		output.append(read)
+SeqIO.write(output, 'converted.fa', 'fasta')
+print('Execution completed successfully')
